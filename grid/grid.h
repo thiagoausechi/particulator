ade748d4c11ef4087d4cc5ae08141d9ccb1b0ecc
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-02-11 20:49:16 -0300

    feat: ✨ add Grid class with initialization and clearing functionality

---
+++ grid/grid.h
@@ -0,0 +1,35 @@
+#ifndef GRID_H
+#define GRID_H
+
+#include <set>
+
+class Grid {
+    int width, height;
+    std::vector<int> grid;
+    std::set<int> modifiedIndices;
+    bool cleared;
+    int rowCount;
+
+public:
+    Grid(): width(0), height(0), cleared(false), rowCount(0) {
+    } ;
+
+    void clear() {
+        const int size = this->width * this->height;
+        this->grid = std::vector(size, 0);
+        this->cleared = true;
+    }
+
+    void init(const int width, const int height) {
+        this->width = width;
+        this->height = height;
+        this->clear();
+        this->modifiedIndices.clear();
+        this->cleared = false;
+        this->rowCount = this->grid.size() / this->width;
+    }
+
+    ~Grid() = default;
+};
+
+#endif //GRID_H
