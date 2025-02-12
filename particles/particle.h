f4cba1fc1a92546206486764d86bb7c2a81cb81a
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-02-12 14:25:24 -0300

    feat: ✨ add Particle class and properties for particle management

---
+++ particles/particle.h
@@ -0,0 +1,44 @@
+#ifndef PARTICLE_H
+#define PARTICLE_H
+
+#include <allegro5/allegro_color.h>
+
+struct ParticleProperties {
+    ALLEGRO_COLOR color;
+    bool empty;
+    bool solid;
+
+    explicit ParticleProperties(const ALLEGRO_COLOR color) : color(color), empty(false), solid(true) {
+    }
+
+    explicit ParticleProperties(const ALLEGRO_COLOR color, const bool empty) : color(color), empty(empty), solid(true) {
+    }
+
+    ParticleProperties(const ALLEGRO_COLOR color, const bool empty, const bool solid) : color(color), empty(empty),
+        solid(solid) {
+    }
+
+    ~ParticleProperties() = default;
+};
+
+class Particle {
+protected:
+    int index;
+    ParticleProperties properties;
+
+public:
+    Particle(const int index, const ParticleProperties &properties) : index(index), properties(properties) {
+    }
+
+    void updateIndex(const int index) {
+        this->index = index;
+    }
+
+    ParticleProperties getProperties() const {
+        return this->properties;
+    }
+
+    ~Particle() = default;
+};
+
+#endif //PARTICLE_H
