b05fea6e631504af8689c98b62ac555e4ee84ad0
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-02-13 15:26:30 -0300

    fix: 🚨 add warning for `update` method not implemented in `Behavior` class

--- behaviors/behavior.h
+++ behaviors/behavior.h
@@ -9,7 +9,9 @@ class Grid;
 
 class Behavior {
 public:
-    void update(std::shared_ptr<Particle> particle, Grid &grid);
+    void update(std::shared_ptr<Particle> particle, Grid &grid) {
+        fprintf(stderr, "Behavior::update() not implemented\n");
+    };
 };
 
 #endif //BEHAVIOR_H
