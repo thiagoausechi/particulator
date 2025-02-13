4e3b72ef563ce5d88deac36dbdb2d180fdc47030
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-02-13 16:01:42 -0300

    feat: ✨ add SandParticle class

---
+++ particles/sand_particle.h
@@ -0,0 +1,19 @@
+#ifndef SAND_PARTICLE_H
+#define SAND_PARTICLE_H
+
+#include "behavior.h"
+#include "moves_down_behavior.h"
+#include "particle.h"
+#include "colors.h"
+
+class SandParticle final : public Particle {
+public:
+    explicit SandParticle(const int index) : Particle(
+        index,
+        ParticleProperties()
+            .setColor(Colors::varyColor({40.0, 65.2, 60.6}))
+            .setBehaviors({std::make_shared<MovesDownBehavior>()})) {
+    }
+};
+
+#endif // SAND_PARTICLE_H
