79957b83ad87ddc8624cff9d6cee6e9341f5e9c2
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-02-14 08:54:58 -0300

    feat: ✨ implement MovesDown behavior for particle movement and remove deprecated MovesDownBehavior

---
+++ behaviors/moves_down.h
@@ -0,0 +1,120 @@
+#ifndef MOVES_DOWN_H
+#define MOVES_DOWN_H
+
+#include "behavior.h"
+
+class MovesDown : public Behavior {
+public:
+    MovesDown(const float maxSpeed, const float acceleration) : maxSpeed(maxSpeed), acceleration(acceleration),
+                                                                velocity(0) {
+    }
+
+    void update(std::shared_ptr<Particle> particle, Grid &grid, const UpdateParams &params) override {
+        const auto index = particle->getIndex();
+
+        this->updateVelocity();
+        const auto updateCount = this->getUpdateCount();
+
+        for (int v = 0; v < updateCount; v++) {
+            this->step(particle, grid);
+
+            if (const auto newIndex = particle->getIndex(); newIndex != index) {
+                grid.onModified(index);
+                grid.onModified(newIndex);
+            } else {
+                this->resetVelocity();
+                break;
+            }
+        }
+    }
+
+protected:
+    float maxSpeed;
+    float acceleration;
+    float velocity;
+
+    void resetVelocity() {
+        this->velocity = 0;
+    }
+
+    void updateVelocity() {
+        this->velocity = this->nextVelocity();
+    }
+
+    [[nodiscard]] float nextVelocity() const {
+        if (this->maxSpeed == 0)
+            return 0;
+
+        auto newVelocity = this->velocity + this->acceleration;
+        if (std::abs(newVelocity) > this->maxSpeed)
+            newVelocity = std::copysign(maxSpeed, newVelocity);
+        return newVelocity;
+    }
+
+    [[nodiscard]] int getUpdateCount() const {
+        const auto abs = std::abs(this->velocity);
+        const auto floored = std::floor(abs);
+        const auto mod = abs - floored;
+
+        return static_cast<int>(floored + (randomInRange(0, 1) < mod ? 1 : 0));
+    }
+
+    [[nodiscard]] bool canPassThrough(const std::shared_ptr<Particle> &particle) const {
+        if (!particle)
+            return false;
+        const auto properties = particle->getProperties();
+        return properties.empty || !properties.solid;
+    }
+
+    [[nodiscard]] auto possibleMoves(const Grid &grid, const int index) const {
+        const int nextDelta = std::copysign(grid.getWidth(), this->velocity);
+        const auto
+                nextVertical = index + nextDelta,
+                nextVerticalLeft = nextVertical - 1,
+                nextVerticalRight = nextVertical + 1;
+
+        const auto column = nextVertical % grid.getWidth();
+
+        std::vector<int>
+                moves = {},
+                weights = {};
+
+        if (this->canPassThrough(grid.particleAt(nextVertical))) {
+            moves.push_back(nextVertical);
+            weights.push_back(2);
+        } else {
+            if (this->canPassThrough(grid.particleAt(nextVerticalLeft))
+                && nextVerticalLeft % grid.getWidth() < column) {
+                moves.push_back(nextVerticalLeft);
+                weights.push_back(1);
+            }
+
+            if (this->canPassThrough(grid.particleAt(nextVerticalRight))
+                && nextVerticalRight % grid.getWidth() > column) {
+                moves.push_back(nextVerticalRight);
+                weights.push_back(1);
+            }
+        }
+
+        return std::pair{moves, weights};
+    }
+
+    void step(const std::shared_ptr<Particle> &particle, Grid &grid) {
+        const auto index = particle->getIndex();
+        if (grid.isEmpty(index)) {
+            this->resetVelocity();
+            return;
+        }
+
+        const auto [moves, weights] = this->possibleMoves(grid, index);
+        if (moves.empty()) {
+            this->resetVelocity();
+            return;
+        }
+
+        const auto choice = choose(moves, weights);
+        grid.swap(index, choice);
+    }
+};
+
+#endif //MOVES_DOWN_H
--- particles/sand_particle.h
+++ particles/sand_particle.h
@@ -1,8 +1,6 @@
 #ifndef SAND_PARTICLE_H
 #define SAND_PARTICLE_H
 
-#include "behavior.h"
-#include "moves_down_behavior.h"
 #include "particle.h"
 #include "colors.h"
 
@@ -11,7 +9,7 @@ public:
     explicit SandParticle() : Particle(
         ParticleProperties()
             .setColor(Colors::varyColor({40.0, 65.2, 60.6}))
-            .setBehaviors({std::make_shared<MovesDownBehavior>()})) {
+        ) {
     }
 };
 
