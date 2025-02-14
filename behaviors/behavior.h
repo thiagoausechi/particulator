2173cb95abea94bc7f12f041c8c38ca004cf3ac2
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-02-14 08:52:29 -0300

    refactor: ♻️ update Behavior class to include optional parameters in update method

--- behaviors/behavior.h
+++ behaviors/behavior.h
@@ -2,14 +2,15 @@
 #define BEHAVIOR_H
 
 #include <cstdio>
-#include <vector>
+#include <memory>
+#include "grid.h"
 
 class Particle;
 class Grid;
 
 class Behavior {
 public:
-    virtual void update(std::shared_ptr<Particle> particle, Grid &grid) {
+    virtual void update(std::shared_ptr<Particle> particle, Grid &grid, const UpdateParams &params = {}) {
         fprintf(stderr, "Behavior::update() not implemented\n");
     }
 
