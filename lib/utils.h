9d358eed11ff63437b925f3b85b1715d3d59e319
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-02-14 08:38:06 -0300

    feat: ✨ add choose function for weighted random selection from an array

--- lib/utils.h
+++ lib/utils.h
@@ -10,4 +10,22 @@ inline int randomInRange(const int min, const int max) {
     return dis(gen);
 }
 
+template<typename T>
+T choose(std::vector<T> array, std::vector<int> weights) {
+    if (array.size() != weights.size())
+        throw std::runtime_error("Array and weights must be the same length");
+
+    const auto sum = std::accumulate(weights.begin(), weights.end(), 0);
+    const auto randomNumber = randomInRange(0, sum);
+    int threshold = 0;
+
+    for (int i = 0; i < array.size(); ++i) {
+        threshold += weights[i];
+        if (threshold >= randomNumber)
+            return array[i];
+    }
+
+    throw std::runtime_error("Something went wrong");
+}
+
 #endif //UTILS_H
